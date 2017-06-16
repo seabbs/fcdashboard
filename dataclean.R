8490a17875d8daa7928a98bb11e7b1f8ba42d242
Sam Abbott <s.e.abbott12@gmail.com>
2017-06-16 12:16:16 +0100

    updated to new dplyr

--- dataclean.R
+++ dataclean.R
@@ -17,7 +17,7 @@ loanbook <- loanbook %>%
 ## Change variables into factors when ordering is important
 ## Make sure strings do not have free text
 loanbook <- loanbook %>% 
-  mutate_at(.cols = c('status','credit_band',
+  mutate_at(.vars = c('status','credit_band',
                       'loan_purpose', 'sector', 
                       'business_type_name', 'region_name',
                       'whole_loan', 'repayment_type', 
