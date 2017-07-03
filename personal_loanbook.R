e2bd8b4461860114c15d68019d427341ba855d2c
Sam Abbott <s.e.abbott12@gmail.com>
2017-07-03 13:42:34 +0000

    added initial code to link datasets

--- personal_loanbook.R
+++ personal_loanbook.R
@@ -1,5 +1,6 @@
+library(tidyverse)
 
-
+source("dataclean.R")
 #' Dataset by loan
 #' Data set contains:
 #' - the number of loan parts owned
@@ -18,6 +19,7 @@
 #' - no additional information on how much loans are for sale for etc
 #' 
 #' Data upload button option, for testing have data in local repo (remove when uploaded to server)
+#' Need to add  option to upload complete loanbook as well if it is out of date.
 #' 
 #' Data munging:
 #' - Load data
@@ -44,3 +46,21 @@
 #'   - For comparisions need to use proportions or will not be comparable - reuse graphs but with proportions if possible
 #'   - Sub tab PCA your data set main dataset
 #'   - Bad debt interest by number of loans and sample from loanbook, add option to sample based on your distribution
+
+
+personal_loanbook <- read_csv("personal_loanbook.csv")
+
+bind_loanbooks <- function(personal_loanbook, fc_loanbook, verbose= TRUE) {
+  personal_loanbook <- personal_loanbook %>% 
+  rename(id = `Loan ID`)
+
+combined_loanbook <- fc_loanbook %>% 
+  full_join(personal_loanbook)
+
+if (verbose) {
+  loans_without_data <- is.na(combined_loanbook$credit_band) %>% sum
+  message("Loan books are bound with ", loans_without_data, " missing loan entries. Considering uploading an updated funding circle loan book")
+}
+return(combined_loanbook)
+}
+
