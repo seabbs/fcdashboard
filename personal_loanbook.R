4dc74e87fd5c2b71f98e6db2c21a4c8e45cc2f44
Sam Abbott <s.e.abbott12@gmail.com>
2017-07-03 12:40:39 +0000

    Added framework plan for adding personal loanbook breakdown

--- personal_loanbook.R
+++ personal_loanbook.R
@@ -15,4 +15,31 @@
 #' - same as above but converted into individual status (i.e what if one part is for sale
 #' and another is not)
 #' - contains the amount for each loan part. could own the same loan with parts of different amounts
-#' - no additional informatio on how much loans are for sale for etc
+#' - no additional information on how much loans are for sale for etc
+#' 
+#' 
+#' Data munging:
+#' - Load data
+#' - Rename unique varibles
+#' - Full join to funding circle loanbook
+#' - If number of loan parts is NA add variable saying not in personal loanbook
+#' if it has a value then declare it to be in personal loan book
+#' 
+#' Data visualisation: Implement tab 1 and tabs reusing code and release. Then implement comparision with FC loanbook and release
+#' Then add improvements from list and feddback.
+#' - First tab - Personal dashboard:
+#'   - option to change bad debt estimates source from FC.
+#'   - First table is a table of your data
+#'   - Second table summarises your data (skimr table)
+#'   - Lollipop chart of variable statified category y axis is either loan ampount held, or number of parts, proportion of amount lent
+#'   - Loan repayment schedule (not stratified) and same graph stratified.
+#'   - Button information: amount lent, crude interest rate, expected interest after fees and bad debt
+#' - Second tab - Explore data set:
+#'   - Reuse code for PCA and exploratory plotting from the FC loan book as 2 and third tabs
+#' filtering so only loans in personal loanbook are presented and loan amount is set to be the 
+#' amount that you hold. Add filter to switch between loan book amount and the amount you hold.
+#' - Third tab: - 
+#'   - Comparing personal loanbook and fc loan book
+#'   - For comparisions need to use proportions or will not be comparable - reuse graphs but with proportions if possible
+#'   - Sub tab PCA your data set main dataset
+#'   - Bad debt interest by number of loans and sample from loanbook, add option to sample based on your distribution
