8d2a6b238514a483e471cbbda61b75e214aed8af
Sam Abbott <s.e.abbott12@gmail.com>
2017-07-30 14:37:41 +0100

    added repayments made to main loanbook

--- clean_fc_loanbook.R
+++ clean_fc_loanbook.R
@@ -39,8 +39,11 @@ load_clean_loanbook <- function(loanbook_path) {
                               'C (Average risk)', 
                               'D', 
                               'E')
-             )
-    )
+             ))
+           
+    ## Add repayments made
+    loanbook <- loanbook %>% 
+      mutate(repayments_made = term - payments_remaining)
   
   ## Change loan term to an ordinal factor
   loanbook <- loanbook %>% 
